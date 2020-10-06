ca60d5a274dd380340b9b4985cab578f19a33fb9
Shin Kobara <shin.kobara@gmail.com>
2020-10-06 12:12:16 -0500

    update vue

--- nuxt.config.js
+++ nuxt.config.js
@@ -103,7 +103,7 @@ module.exports = {
       },
       {
         /* jQuery for ajax */
-        src: "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"
+        src: "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"
       },
       {
         src: "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"
